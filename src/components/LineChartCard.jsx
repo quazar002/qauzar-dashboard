2042cdd1d87ad1b5e3ff64a1b11f81fc740892e3
Jamshid <quazar002@quazar.co.kr>
2025-04-30 16:49:26 +0900

    line chat name change

--- src/components/LineChartCard.jsx
+++ src/components/LineChartCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import '../styles/LineChartCard_temp.css';
+import '../styles/LineChartCard.css';
 
 export default function LineChartCard() {
   const [selectedRange, setSelectedRange] = useState('12M');
