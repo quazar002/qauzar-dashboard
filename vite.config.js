63f87edb4969b5f428ac0c48e69da9a46c4f2e05
Jamshid <quazar002@quazar.co.kr>
2025-04-30 17:15:38 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: path.join(__dirname, 'src'),
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || '/qauzar-dashboard',
 })
